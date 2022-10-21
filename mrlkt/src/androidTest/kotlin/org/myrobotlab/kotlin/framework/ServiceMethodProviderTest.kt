f2174369564a12848426829d3d44e77771049bd0
Branden Butler <bwtbutler@hotmail.com>
2022-10-21 10:48:37 -0500

    Add additional test cases for executeMethod()

--- mrlkt/src/androidTest/kotlin/org/myrobotlab/kotlin/framework/ServiceMethodProviderTest.kt
+++ mrlkt/src/androidTest/kotlin/org/myrobotlab/kotlin/framework/ServiceMethodProviderTest.kt
@@ -3,18 +3,38 @@ package org.myrobotlab.kotlin.framework
 import org.myrobotlab.kotlin.framework.ServiceMethodProvider.callMethod
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class TestService: Service("test") {
     fun testMethod(): String {
         return "test"
     }
+
+    fun testMethodWithParams(param1: String): String {
+        return "test: $param1"
+    }
 }
 
 class ServiceMethodProviderTest {
     @Test
-    internal fun testExecuteMethod() {
+    internal fun testExecuteMethodNoParams() {
         val t = TestService()
         val r = t.callMethod("testMethod", listOf())
         assertEquals("test", r)
+    }
+
+    @Test
+    internal fun testExecuteMethodOneParam() {
+        val t = TestService()
+        val r = t.callMethod("testMethodWithParams", listOf("test1"))
+        assertEquals("test: test1", r)
+    }
+
+    @Test
+    internal fun testExecuteMethodOneParamWithNull() {
+        val t = TestService()
+        assertFailsWith(RuntimeException::class) {
+            t.callMethod("testMethodWithParams", listOf(null))
+        }
     }
 }
