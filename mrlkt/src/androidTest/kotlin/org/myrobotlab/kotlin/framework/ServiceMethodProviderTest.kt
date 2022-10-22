c1d077d4b2a8a24266c24aafa9402dbf33b89e8b
Branden Butler <bwtbutler@hotmail.com>
2022-10-22 14:35:59 -0500

    Move TestService out of top-level scope

--- mrlkt/src/androidTest/kotlin/org/myrobotlab/kotlin/framework/ServiceMethodProviderTest.kt
+++ mrlkt/src/androidTest/kotlin/org/myrobotlab/kotlin/framework/ServiceMethodProviderTest.kt
@@ -5,17 +5,19 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
-class TestService: Service("test") {
-    fun testMethod(): String {
-        return "test"
-    }
 
-    fun testMethodWithParams(param1: String): String {
-        return "test: $param1"
-    }
-}
 
 class ServiceMethodProviderTest {
+    class TestService: Service("test") {
+        fun testMethod(): String {
+            return "test"
+        }
+
+        fun testMethodWithParams(param1: String): String {
+            return "test: $param1"
+        }
+    }
+
     @Test
     internal fun testExecuteMethodNoParams() {
         val t = TestService()
