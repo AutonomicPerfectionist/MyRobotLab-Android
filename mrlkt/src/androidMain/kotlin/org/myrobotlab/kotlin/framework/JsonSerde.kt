76233613ee623be01520bce72c4c6c524ba53f84
Branden Butler <bwtbutler@hotmail.com>
2022-10-23 17:16:06 -0500

    Make JsonSerde expect/actual

---
+++ mrlkt/src/androidMain/kotlin/org/myrobotlab/kotlin/framework/JsonSerde.kt
@@ -0,0 +1,11 @@
+package org.myrobotlab.kotlin.framework
+
+actual class JsonSerde {
+    actual fun <T> deserialize(json: String): T {
+        TODO("Not yet implemented")
+    }
+
+    actual fun <T> serialize(o: T): String {
+        TODO("Not yet implemented")
+    }
+}
