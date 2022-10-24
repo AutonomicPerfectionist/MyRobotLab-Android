670b3c969589da08393d9539b442143db20d5559
Branden Butler <bwtbutler@hotmail.com>
2022-10-24 17:55:20 -0500

    Implement mappings in JVM with Reflections

--- mrlkt/src/androidMain/kotlin/org/myrobotlab/kotlin/framework/mappings.kt
+++ mrlkt/src/androidMain/kotlin/org/myrobotlab/kotlin/framework/mappings.kt
@@ -1,27 +1,17 @@
 package org.myrobotlab.kotlin.framework
 
-import okhttp3.internal.toImmutableMap
-import org.myrobotlab.kotlin.framework.MappingInitializer.mutableMapping
 import org.myrobotlab.kotlin.utils.BiMap
 import org.myrobotlab.kotlin.utils.HashBiMap
-import org.myrobotlab.kotlin.utils.MutableBiMap
 import org.reflections.Reflections
 import kotlin.reflect.full.findAnnotation
 
 
-actual val mappings: BiMap<String, String>
-    get() = mutableMapping
-
-
-
-private object MappingInitializer {
-    val mutableMapping = HashBiMap<String, String>()
-
-    init {
-        val reflections = Reflections("org.myrobotlab")
-        val annotated = reflections.getTypesAnnotatedWith(MrlClassMapping::class.java)
-        annotated.forEach {
-            mutableMapping[it.kotlin.qualifiedName!!] = it.kotlin.findAnnotation<MrlClassMapping>()!!.javaClass
-        }
+actual val mappings: BiMap<String, String> = HashBiMap<String, String>().apply {
+    val reflections = Reflections("org.myrobotlab")
+    val annotated = reflections.getTypesAnnotatedWith(MrlClassMapping::class.java, true)
+    annotated.forEach {
+        val qualifiedName = it.kotlin.qualifiedName ?: return@forEach
+        val javaClass = it.kotlin.findAnnotation<MrlClassMapping>()?.javaClass ?: return@forEach
+        this[qualifiedName] = javaClass
     }
 }
