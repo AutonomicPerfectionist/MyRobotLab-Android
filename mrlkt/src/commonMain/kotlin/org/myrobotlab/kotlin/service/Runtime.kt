959e6d672531833dad771b0800d0b6527e74e45f
Branden Butler <bwtbutler@hotmail.com>
2022-10-27 21:30:48 -0500

    Implement Runtime registry and id

--- mrlkt/src/commonMain/kotlin/org/myrobotlab/kotlin/service/Runtime.kt
+++ mrlkt/src/commonMain/kotlin/org/myrobotlab/kotlin/service/Runtime.kt
@@ -1,6 +1,32 @@
 package org.myrobotlab.kotlin.service
 
+import org.myrobotlab.kotlin.framework.Registration
 import org.myrobotlab.kotlin.framework.Service
+import org.myrobotlab.kotlin.utils.ImmutableMapWrapper
 
 object Runtime: Service("runtime") {
+    lateinit var runtimeID: String
+    private set
+
+    private var hasInit = false
+
+    private val mutableRegistry = mutableMapOf<String, Registration>()
+
+    /**
+     * The current registry of services known to MrlKt.
+     * In order to add new elements, one must call [register]
+     */
+    val registry: Map<String, Registration> = ImmutableMapWrapper(mutableRegistry)
+
+    fun initRuntime(id: String) {
+        require(!hasInit) {"Runtime may only be initialized once"}
+        hasInit = true
+        runtimeID = id
+        register(Registration(this))
+    }
+
+
+    fun register(registration: Registration) {
+        mutableRegistry[registration.name] = registration
+    }
 }
