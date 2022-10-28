7267c34b5f23cdcf76ace59d859705c431bcd518
Branden Butler <bwtbutler@hotmail.com>
2022-10-27 21:27:09 -0500

    Inline ProxyMethod.invoke()

--- mrlkt/src/commonMain/kotlin/org/myrobotlab/kotlin/framework/Proxy.kt
+++ mrlkt/src/commonMain/kotlin/org/myrobotlab/kotlin/framework/Proxy.kt
@@ -3,7 +3,7 @@ package org.myrobotlab.kotlin.framework
 import kotlin.reflect.KClass
 
 data class ProxyMethod<R: Any>(val name: String, val methodName: String, val paramList: List<KClass<*>>, val returnType: KClass<R>) {
-    operator fun invoke(vararg params: Any?): R? = MrlClient.callService(name, methodName, params)
+    inline operator fun <reified T: R> invoke(vararg params: Any?): T? = MrlClient.callService(name, methodName, params)
 }
 
 data class Proxy(val name: String, val typeKey: String, val methods: List<ProxyMethod<*>>) {
