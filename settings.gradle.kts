eb65711f9f12349d8672efac460093ff43fdfc2d
Branden Butler <bwtbutler@hotmail.com>
2022-11-06 08:59:03 -0600

    Add new modules to be included

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,9 +10,14 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 
 rootProject.name = "MyRobotLab_Mobile"
 include(":MyRobotLab_Android")
+include(":mrlkt-ksp")
 include(":mrlkt")
+
+include(":mrlkt-annotations")
+include(":mrlkt-ksp")
