3c7bbc436de23999047f9df4e6b87b4ff93ff1bd
Branden Butler <bwtbutler@hotmail.com>
2022-11-06 08:58:20 -0600

    Switch mrlkt-annotations to multiplatform module

--- mrlkt-annotations/build.gradle.kts
+++ mrlkt-annotations/build.gradle.kts
@@ -1,9 +1,21 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    kotlin("multiplatform")
+
+//    id("java-library")
+//    id("org.jetbrains.kotlin.jvm")
+}
+kotlin {
+    ios()
+    iosSimulatorArm64()
+    jvm()
+    sourceSets {
+        val commonMain by getting {
+            kotlin.srcDir("src/main/java")
+        }
+    }
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
-}
+//java {
+//    sourceCompatibility = JavaVersion.VERSION_1_7
+//    targetCompatibility = JavaVersion.VERSION_1_7
+//}
