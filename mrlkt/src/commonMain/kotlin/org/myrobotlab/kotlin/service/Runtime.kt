d74e889bf0b19a348f9c1907780dbc6860e4ca18
Branden Butler <bwtbutler@hotmail.com>
2022-11-20 16:27:36 -0600

    Add registration callbacks to Runtime and add documentation for Runtime.start()

--- mrlkt/src/commonMain/kotlin/org/myrobotlab/kotlin/service/Runtime.kt
+++ mrlkt/src/commonMain/kotlin/org/myrobotlab/kotlin/service/Runtime.kt
@@ -26,12 +26,19 @@ object Runtime: Service("runtime") {
         require(!hasInit) {"Runtime may only be initialized once"}
         hasInit = true
         runtimeID = id
-        register(Registration(runtimeID, "runtime", "org.myrobotlab.service.Runtime"))
+        mutableRegistry["runtime"] = Registration(runtimeID, "runtime", "org.myrobotlab.service.Runtime")
     }
 
 
-    fun register(registration: Registration) {
+    suspend fun register(registration: Registration) {
+        MrlClient.logger.info("Registering: ${registration.name}")
         mutableRegistry[registration.name] = registration
+        invoke<Registration>("registered", registration)
+    }
+
+    suspend fun registered(registration: Registration): Registration {
+        MrlClient.logger.info("Registered service: ${registration.name}@${registration.id}")
+        return registration
     }
 
     fun describe(uuid: String, query: String): DescribeResults {
@@ -39,18 +46,31 @@ object Runtime: Service("runtime") {
         return DescribeResults(runtimeID, "", mapOf(), null, null, registry.values.toList())
     }
 
+    /**
+     * Start a new service with the provided name. Which
+     * service is started is determined by [R]
+     *
+     * @param R The Service type to create and start
+     * @param name The name of the new service
+     */
     suspend inline fun <reified R: Service> start(name: String): R? {
         return start(name, R::class)
     }
 
+    /**
+     * Start a new service of the provided type with the provided name.
+     *
+     * @param name The name of the new service.
+     * @param type Which service class to instantiate.
+     */
     suspend fun <R : ServiceInterface> start(name: String, type: KClass<R>): R? = coroutineScope {
-        val old = Runtime.registry[name]?.service ?: suspend {
+        MrlClient.logger.info("Starting service $name")
+        val old = Runtime.registry[name]?.service ?: this.run {
+            MrlClient.logger.info("Creating service")
             val service = type.constructService(name)
             service.start(this)
-            Runtime.invoke<Unit>("register",
-                Registration(runtimeID,
-                    name,
-                    "kt:${type.qualifiedName}", service = service))
+            register(Registration(service))
+            return@run service
 
         }
         if (old::class == type) {
