b61a98cc014cb6680bef07a1a8dcf367fecf7ba9
Branden Butler <bwtbutler@hotmail.com>
2022-11-28 19:26:36 -0600

    Add ContextContainer

---
+++ MyRobotLab_Android/src/main/java/org/myrobotlab/android/ContextContainer.kt
@@ -0,0 +1,7 @@
+package org.myrobotlab.android
+
+import android.content.Context
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+data class ContextContainer(val context: Context)
