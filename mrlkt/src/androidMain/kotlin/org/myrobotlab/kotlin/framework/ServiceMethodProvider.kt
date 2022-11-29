57d445d417228cdcedb48c635da3c2b2ea00ede3
Branden Butler <bwtbutler@hotmail.com>
2022-11-29 16:42:51 -0600

    Enhance error message when no compatible method found

--- mrlkt/src/androidMain/kotlin/org/myrobotlab/kotlin/framework/ServiceMethodProvider.kt
+++ mrlkt/src/androidMain/kotlin/org/myrobotlab/kotlin/framework/ServiceMethodProvider.kt
@@ -75,7 +75,7 @@ actual object ServiceMethodProvider {
         }
 
         if (compatibleServiceMethod == null) {
-            throw RuntimeException("Cannot find compatible method")
+            throw RuntimeException("Cannot find compatible method ($name@$method($data))")
         }
         // TODO Test whether receiver is needed, test whether method is suspending or not
         if (compatibleServiceMethod.isSuspend) {
