aa93745b5b080a89e9dfbe0083468b6d0837949d
Branden Butler <bwtbutler@hotmail.com>
2022-11-29 20:59:02 -0600

    Add missing class mapping to Orientation

--- mrlkt/src/commonMain/kotlin/org/myrobotlab/kotlin/service/data/Orientation.kt
+++ mrlkt/src/commonMain/kotlin/org/myrobotlab/kotlin/service/data/Orientation.kt
@@ -1,3 +1,6 @@
 package org.myrobotlab.kotlin.service.data
 
+import org.myrobotlab.kotlin.annotations.MrlClassMapping
+
+@MrlClassMapping("org.myrobotlab.service.data.Orientation")
 data class Orientation(val roll: Double, val pitch: Double, val yaw: Double)
