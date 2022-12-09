31b1829a3aa6d50200e26540aa8e77ce851285f6
Branden Butler <bwtbutler@hotmail.com>
2022-12-08 23:33:31 -0600

    Implement Sampler, update documentation, remove samplePeriod in favor of sampleRate

--- MyRobotLab_Android/src/main/java/org/myrobotlab/android/service/Gyro.kt
+++ MyRobotLab_Android/src/main/java/org/myrobotlab/android/service/Gyro.kt
@@ -10,18 +10,42 @@ import org.koin.core.component.inject
 import org.myrobotlab.android.ContextContainer
 import org.myrobotlab.kotlin.annotations.MrlService
 import org.myrobotlab.kotlin.framework.Service
-import org.myrobotlab.kotlin.service.data.Orientation
+import org.myrobotlab.kotlin.service.data.*
 import org.myrobotlab.kotlin.service.interfaces.OrientationListener
 import org.myrobotlab.kotlin.service.interfaces.OrientationPublisher
+import org.myrobotlab.kotlin.service.interfaces.Sampler
 
+/**
+ * A service that provides access to the Android Rotation
+ * Vector sensor, which in essence acts as a software-normalized
+ * gyroscope instead of raw gyro data.
+ *
+ * [Orientation] data published by this service describes
+ * the orientation of the physical Android device this service is
+ * running on. On a normal candy-bar style phone,
+ * this defines the Z axis as being parallel to the device's
+ * long edge and going down the center. The X axis is therefore
+ * parallel to the short edge through the center, and the Y axis is
+ * through the device from back to front through the center again.
+ *
+ * These axes may be different on other Android devices.
+ *
+ * See [Orientation] for information on how the rotational
+ * axes are defined in relation to the x/y/z axes.
+ *
+ * The orientation angles range from -180 to +180
+ * with zero pitch and roll being defined as the standard
+ * perfectly vertical Portrait orientation. Yaw's
+ * zero is harder to define, it is recommended to find it by experimentation,
+ *
+ * @author AutonomicPerfectionist
+ */
 @MrlService
-class Gyro(name: String): Service(name), OrientationPublisher, SensorEventListener {
-    private val contextContainer: ContextContainer by inject()
+class Gyro(name: String): Service(name), OrientationPublisher, SensorEventListener, Sampler {
     private val sensorManager: SensorManager by inject()
     private var gyro: Sensor? = null
 
-    private val sensorPollRate = 500 * 1000 //500 ms
-
+    override var sampleRate: Hertz = 2.hz
 
 
     override fun publishOrientation(data: Orientation): Orientation {
@@ -38,7 +62,7 @@ class Gyro(name: String): Service(name), OrientationPublisher, SensorEventListen
 
     override fun start() {
         gyro = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
-        sensorManager.registerListener(this, gyro, sensorPollRate)
+        sensorManager.registerListener(this, gyro, sampleRate.period.toMicroseconds().value)
 
     }
 
@@ -46,16 +70,20 @@ class Gyro(name: String): Service(name), OrientationPublisher, SensorEventListen
         sensorManager.unregisterListener(this)
     }
 
+    @Deprecated("use start", ReplaceWith("start()"))
     override fun startOrientationTracking() {
         TODO("Not yet implemented")
     }
 
+    @Deprecated("use stop", ReplaceWith("stop()"))
     override fun stopOrientationTracking() {
         TODO("Not yet implemented")
     }
 
     override fun onSensorChanged(event: SensorEvent) {
         if (event.sensor == gyro) {
+            // Big long calculation to convert
+            // quaternion to yaw/pitch/roll
             val rotationMatrix = FloatArray(9)
             SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values)
             val worldAxisX = SensorManager.AXIS_X
@@ -69,9 +97,9 @@ class Gyro(name: String): Service(name), OrientationPublisher, SensorEventListen
             )
             val orientationArray = FloatArray(3)
             SensorManager.getOrientation(adjustedRotationMatrix, orientationArray)
+            val yaw: Float = orientationArray[0] * FROM_RADS_TO_DEGS
             val pitch: Float = orientationArray[1] * FROM_RADS_TO_DEGS
             val roll: Float = orientationArray[2] * FROM_RADS_TO_DEGS
-            val yaw: Float = orientationArray[0] * FROM_RADS_TO_DEGS
             val orientation = Orientation(roll.toDouble(), pitch.toDouble(), yaw.toDouble())
             serviceScope.launch {
                 invoke<Orientation>("publishOrientation", orientation)
